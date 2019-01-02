fe1380a79fbaee69df3bf517d880fab58a8e4b2e
James Patillo <james.patillo@gmail.com>
2019-01-02 17:09:37 -0500

    changed debug value

--- headers/main.h
+++ headers/main.h
@@ -3,7 +3,7 @@
 
 #define DHT11_PIN 3
 #define RELAY_PIN 4
-#define DEFAULT_ENVIRONMENT_INTERVAL 600; // 10 minutes
+#define DEFAULT_ENVIRONMENT_INTERVAL 10//600; // 10 minutes
 
 
 void setRelayPin(unsigned int pin);
