62446e74eb11b7d0875d8d04a62fd72b17a00faa
James Patillo <james.patillo@gmail.com>
2019-03-03 12:06:53 -0500

    improvements

--- headers/main.h
+++ headers/main.h
@@ -3,8 +3,17 @@
 
 #define DHT11_PIN 3
 #define RELAY_PIN 4
-#define DEFAULT_ENVIRONMENT_INTERVAL 10//600; // 10 minutes
+#define DEFAULT_ENVIRONMENT_INTERVAL 600 // 10 minutes
 
+typedef struct
+{
+    int mqtt_port;
+    char* mqtt_host;
+    int mqtt_keepalive;
+    unsigned int ports_dht11;
+    unsigned int ports_relay;
+    int env_update;
+} configuration;
 
 void setRelayPin(unsigned int pin);
 void activateRelay(unsigned int pin);
