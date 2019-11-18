86ee13ff92fafae99ba897fd920fb6fcbd08a6f5
James Patillo <james.patillo@gmail.com>
2019-11-18 15:11:22 -0500

    Attempt to handle random disconnects

--- source/mqtt.c
+++ source/mqtt.c
@@ -7,7 +7,7 @@ struct mosquitto *mqtt_setup(struct mosquitto *mosq, char* host, int port, int k
     mosquitto_lib_init();
     
     mosq = mosquitto_new(NULL,true,NULL);
-    if(!mosq){
+    if(!mosq) {
             fprintf(stderr, "Error: Out of memory.\n");
             return NULL;
         }
@@ -15,6 +15,7 @@ struct mosquitto *mqtt_setup(struct mosquitto *mosq, char* host, int port, int k
     mosquitto_connect_callback_set(mosq, mqtt_connect_callback);
     mosquitto_message_callback_set(mosq, mqtt_message_callback);
     mosquitto_subscribe_callback_set(mosq, mqtt_subscribe_callback);
+    mosquitto_disconnect_callback_set(mosq,mqtt_disconnect_callback);
 
     if(mosquitto_connect_async(mosq, host, port, keepalive)){
         fprintf(stderr, "Unable to connect.\n");
@@ -76,6 +77,22 @@ void mqtt_connect_callback(struct mosquitto *mosq, void *userdata, int result)
 	}else{
 		fprintf(stderr, "Connect failed\n");
 	}
+}
+
+void mqtt_disconnect_callback(struct mosquitto *mosq, void *obj, int result)
+{
+    if(result) // result is 0 iff we requested the disconnect
+    {
+        switch(mosquitto_reconnect_async(mosq))
+        {
+            case MOSQ_ERR_SUCCESS: printf("Reconnected to broker");
+            break;
+            case MOSQ_ERR_INVAL: printf("Invalid parameters while trying to reconnect to broker.");
+            break;
+            case MOSQ_ERR_ERRNO: printf("Error reconnecting to broker.\n%s",strerror_r());
+            break;
+        }
+    }
 }
 
 void mqtt_subscribe_callback(struct mosquitto *mosq, void *userdata, int mid, int qos_count, const int *granted_qos)
