731414ee5ba9cc7e4f316b8e30cf4c22128a5011
James Patillo <james.patillo@gmail.com>
2020-01-02 10:26:12 -0500

    fixed missing semicolon

--- headers/mqtt.h
+++ headers/mqtt.h
@@ -6,6 +6,6 @@ struct mosquitto *mqtt_setup(struct mosquitto *mosq, char* host, int port, int k
 void mqtt_cleanup(struct mosquitto *mosq);
 void mqtt_message_callback(struct mosquitto *mosq, void *userdata, const struct mosquitto_message *message);
 void mqtt_connect_callback(struct mosquitto *mosq, void *userdata, int result);
-void mqtt_disconnect_callback(struct mosquitto *mosq, void *obj, int result)
+void mqtt_disconnect_callback(struct mosquitto *mosq, void *obj, int result);
 void mqtt_subscribe_callback(struct mosquitto *mosq, void *userdata, int mid, int qos_count, const int *granted_qos);
 void mqtt_log_callback(struct mosquitto *mosq, void *userdata, int level, const char *str);
