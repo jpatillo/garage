1764216c3c6656968359ef48fdc3576764283c92
James Patillo <james.patillo@gmail.com>
2020-01-11 17:25:53 -0500

    reorganized code to get rid of header dependence

--- source/mqtt.c
+++ source/mqtt.c
@@ -1,55 +1,33 @@
-#include "../headers/mqtt.h"
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+#include <mosquitto.h>
+#include "../headers/util.h"
 
-//TODO: it may not be necessary to have mosq as a param. The reference had to be returned because the param was not retaining the reference.
-struct mosquitto *mqtt_setup(struct mosquitto *mosq, void (*mqtt_message_callback)(struct mosquitto *mosq, void *userdata, const struct mosquitto_message *message)){
-    mosquitto_lib_init();
-    
-    mosq = mosquitto_new("34567",true,NULL);
-    if(!mosq) {
-            fprintf(stderr, "Error: Out of memory.\n");
-            return NULL;
-    }
-    mosquitto_log_callback_set(mosq, mqtt_log_callback);
-    mosquitto_connect_callback_set(mosq, mqtt_connect_callback);
-    mosquitto_message_callback_set(mosq, mqtt_message_callback);
-    mosquitto_subscribe_callback_set(mosq, mqtt_subscribe_callback);
-    mosquitto_disconnect_callback_set(mosq,mqtt_disconnect_callback);
-      
-    return mosq;
-}
+char *mqtt_topic_command_door;
+char *mqtt_topic_command_temperature;
+char *mqtt_topic_command_humidity;
+char *mqtt_topic_status;
+char *mqtt_topic_telemetry;
 
-int mqtt_connect(struct mosquitto *mosq, char* host, int port, int keepalive) {
-    if(mosquitto_connect_async(mosq, host, port, keepalive)){
-        fprintf(stderr, "Unable to connect.\n");
-        return 1;
-    }
-
-    if(mosquitto_loop_start(mosq)) {
-        fprintf(stderr, "Unable to start connection loop.\n");
-        return 1;
-    }
-
-    return 0;
-}
-
-
-void mqtt_cleanup(struct mosquitto *mosq){
-    mosquitto_disconnect(mosq);
-    mosquitto_destroy(mosq);
-	mosquitto_lib_cleanup();
-}
+const char* _mqtt_project_prefix = "garage/";
+const char* _mqtt_suffix_command_door = "/command/door";
+const char* _mqtt_suffix_command_temperature = "/command/temperature";
+const char* _mqtt_suffix_command_humidity = "/command/humidity";
+const char* _mqtt_suffix_status = "/status";
+const char* _mqtt_suffix_telemetry = "/telemetry";
 
 void mqtt_connect_callback(struct mosquitto *mosq, void *userdata, int result)
 {   
 	if(!result){
 		/* Subscribe to broker information topics on successful connect. */
-        mosquitto_subscribe(mosq,NULL,"garage/34567/command/door",1);
-        mosquitto_subscribe(mosq,NULL,"garage/34567/command/humidity",0);
-        mosquitto_subscribe(mosq,NULL,"garage/34567/command/temperature",0);
+        mosquitto_subscribe(mosq,NULL,mqtt_topic_command_door,1);
+        mosquitto_subscribe(mosq,NULL,mqtt_topic_command_humidity,0);
+        mosquitto_subscribe(mosq,NULL,mqtt_topic_command_temperature,0);
 
         printf("Connected to mosquitto with code %i\n",result);
         char* status = "{msg:connected}\0";
-        mosquitto_publish(	mosq, NULL, "garage/34567/status", strlen(status), status, 0, false);
+        mosquitto_publish(	mosq, NULL, mqtt_topic_status, strlen(status), status, 0, false);
 	} else {
 		fprintf(stderr, "Connect failed\n");
 	}
@@ -69,6 +47,12 @@ void mqtt_disconnect_callback(struct mosquitto *mosq, void *obj, int result)
             break;
         }
     }
+
+    free(mqtt_topic_command_door);
+    free(mqtt_topic_command_temperature);
+    free(mqtt_topic_command_humidity);
+    free(mqtt_topic_status);
+    free(mqtt_topic_telemetry);
 }
 
 void mqtt_subscribe_callback(struct mosquitto *mosq, void *userdata, int mid, int qos_count, const int *granted_qos)
@@ -90,3 +74,57 @@ void mqtt_log_callback(struct mosquitto *mosq, void *userdata, int level, const 
     // Get these on a server!
     //mosquitto_publish(	mosq, NULL, "garage/34567/log", strlen(str), str, 0, false);
 }
+
+struct mosquitto *mqtt_setup(struct mosquitto *mosq, char* id, 
+    void (*mqtt_message_callback)(struct mosquitto *mosq, void *userdata, const struct mosquitto_message *message))
+{
+    mosquitto_lib_init();
+    
+    mosq = mosquitto_new(id,true,NULL);
+    if(!mosq) {
+            fprintf(stderr, "Error: Out of memory.\n");
+            return NULL;
+    }
+
+    char* topic_prefix = concat(_mqtt_project_prefix,id);
+    mqtt_topic_command_door = concat(topic_prefix,_mqtt_suffix_command_door);
+    mqtt_topic_command_temperature = concat(topic_prefix,_mqtt_suffix_command_temperature);
+    mqtt_topic_command_humidity = concat(topic_prefix,_mqtt_suffix_command_humidity);
+    mqtt_topic_status = concat(topic_prefix,_mqtt_suffix_status);
+    mqtt_topic_telemetry = concat(topic_prefix,_mqtt_suffix_telemetry);
+    free(topic_prefix);
+
+    mosquitto_log_callback_set(mosq, mqtt_log_callback);
+    mosquitto_connect_callback_set(mosq, mqtt_connect_callback);
+    mosquitto_message_callback_set(mosq, mqtt_message_callback);
+    mosquitto_subscribe_callback_set(mosq, mqtt_subscribe_callback);
+    mosquitto_disconnect_callback_set(mosq,mqtt_disconnect_callback);
+      
+    return mosq;
+}
+
+int mqtt_connect(struct mosquitto *mosq, char* host, int port, int keepalive) {
+    if(mosquitto_connect_async(mosq, host, port, keepalive)){
+        fprintf(stderr, "Unable to connect.\n");
+        return 1;
+    }
+
+    //TODO: under what circumstances is the thread/loop stopped?
+    // Are multiple threads created if this line is run more than once?
+    if(mosquitto_loop_start(mosq)) {
+        fprintf(stderr, "Unable to start connection loop.\n");
+        return 1;
+    }
+
+    return 0;
+}
+
+
+void mqtt_cleanup(struct mosquitto *mosq){
+    mosquitto_disconnect(mosq);
+    mosquitto_destroy(mosq);
+	mosquitto_lib_cleanup();
+}
+
+
+
